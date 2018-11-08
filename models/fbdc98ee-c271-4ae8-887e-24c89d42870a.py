d5325fa24644785ca16c31ed0a7c4cb3aaecd095
tbaer-c7ks7s <tbaer@clockwork-solutions.com>
2018-11-08 15:30:00 -0600

    added a second sortie import component

--- models/fbdc98ee-c271-4ae8-887e-24c89d42870a.py
+++ models/fbdc98ee-c271-4ae8-887e-24c89d42870a.py
@@ -4,14 +4,12 @@ from sqlalchemy import MetaData, Table
 
 metadata = MetaData()
 
-c130_g081_sortie_data = Table('c130_g081_sortie_data', metadata, 
+sortie_import_old = Table('sortie_import_old', metadata, 
 
-    Column('Command', String(4), primary_key=False, autoincrement=False),
-    Column('Possessing_Org', String(9), primary_key=False, autoincrement=False),
     Column('MDS', String(6), primary_key=False, autoincrement=False),
     Column('Serial_Number', String(15), primary_key=True, autoincrement=False),
     Column('Mission_Number', String(20), primary_key=False, autoincrement=False),
-    Column('Mission_Symbol', String(4), primary_key=False, autoincrement=False),
+    Column('Sortie_Number', Integer, primary_key=False, autoincrement=False),
     Column('Depart_ICAO', String(4), primary_key=False, autoincrement=False),
     Column('Depart_Date', DateTime, primary_key=True, autoincrement=False),
     Column('Depart_Time', TIME, primary_key=True, autoincrement=False),
@@ -19,11 +17,8 @@ c130_g081_sortie_data = Table('c130_g081_sortie_data', metadata,
     Column('Land_Date', DateTime, primary_key=False, autoincrement=False),
     Column('Land_Time', TIME, primary_key=False, autoincrement=False),
     Column('Flying_Hours', Float, primary_key=False, autoincrement=False),
-    Column('Sorties', Integer, primary_key=False, autoincrement=False),
+    Column('Sorties_Flown', Integer, primary_key=False, autoincrement=False),
     Column('Total_Landings', Integer, primary_key=False, autoincrement=False),
     Column('Full_Stop_Landings', Integer, primary_key=False, autoincrement=False),
-    Column('Purpose_Possession_Code', String(2), primary_key=False, autoincrement=False),
-    Column('Update_By_BASE', String(4), primary_key=False, autoincrement=False),
-    Column('Update_Date', DateTime, primary_key=False, autoincrement=False),
-    Column('Update_Time', Integer, primary_key=False, autoincrement=False),
+    Column('Sortie_Date', DateTime, primary_key=False, autoincrement=False),
 )
