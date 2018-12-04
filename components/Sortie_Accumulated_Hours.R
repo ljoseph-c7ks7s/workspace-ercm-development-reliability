a477d4cfd1449df925bdd65118c2a50f790b6b87
Thomas Lewis <tlewis@clockwork-solutions.com>
2018-12-04 15:45:10 -0600

    Added Sortie Accumulated Hours

---
+++ components/Sortie_Accumulated_Hours.R
@@ -0,0 +1,17 @@
+fn <- function(conn_rmysql, load_path, params_path, component_path, output_file) {
+  library(dplyr)
+  df_cumulate <- dbGetQuery(conn_rmysql, 'SELECT * FROM compiled_sortie_history_data')
+  df_cumulate[df_cumulate[,"Depart_Date"] < as.Date('1950-01-01'), 5] <- df_cumulate[df_cumulate[,"Depart_Date"] < as.Date('1950-01-01'), 8]
+  df_cumulate <- as.data.frame(df_cumulate)
+  df_cumulate <- df_cumulate[order(df_cumulate$Serial_Number, df_cumulate$Depart_Date),]
+  
+  df_cumulate <- df_cumulate %>%
+    group_by(Serial_Number) %>%
+    mutate(Flying_Hours = cumsum(Flying_Hours))
+  
+  
+  df_cumulate$Depart_Date <-  df_cumulate$Depart_Date %>% substr(1,10) %>% as.POSIXct()
+  df_cumulate$Depart_Time <- substr(df_cumulate$Depart_Time %>% as.POSIXct(format = "%H:%M:%OS"),12,16)
+  df_cumulate$Depart_Date <- as.POSIXct(paste(df_cumulate$Depart_Date, df_cumulate$Depart_Time), format = "%Y-%m-%d %H:%M")
+  df_cumulate$Depart_Date <- df_cumulate$Depart_Date %>% as.character()
+}
