3bb16200c6ff7a3297057147c4b10456f1cfd7b8
tlewis <tlewis@clockwork-solutions.com>
2019-01-29 09:12:59 -0600

    updated r2 script to find r2'd

---
+++ models/2270b339-9c54-4034-85cc-f5a39fba7acf.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, BigInteger, String, DateTime, Text, Float, Date
+from sqlalchemy.dialects.mysql import DOUBLE, DECIMAL, TEXT, MEDIUMTEXT, MEDIUMBLOB, TINYINT, LONGTEXT, BINARY, SMALLINT, MEDIUMINT, BIT, TIMESTAMP, TIME
+from sqlalchemy import MetaData, Table
+
+metadata = MetaData()
+
+one_wuc = Table('one_wuc', metadata, 
+
+    Column('work_unit_code', String(255), primary_key=False, autoincrement=False),
+    Column('transaction_date', Date, primary_key=False, autoincrement=False),
+    Column('discrepancy_narrative', TEXT, primary_key=False, autoincrement=False),
+)
