179e6bb580ef7a785e84d4f33201d13e6bd32e1c
tbaer-c7ks7s <tbaer@clockwork-solutions.com>
2019-02-04 23:10:54 -0600

    drop records with unrelated ATCs

--- scripts/6_Identify_R2.py
+++ scripts/6_Identify_R2.py
@@ -14,15 +14,20 @@ Args:
 
 def fn(conn, libraries, params, predecessors):
     pd = libraries["pandas"]
-    re = libraries["re"]    
+    re = libraries["re"]
+
     df = pd.read_sql(con=conn,sql="SELECT A.*, B.Action_Taken_Code, B.Corrective_Narrative FROM clean_wuc_remove_fom A LEFT JOIN compiled_c130_remis_data B ON A.On_Work_Order_Key=B.On_Work_Order_Key AND A.On_Maint_Action_Key=B.On_Maint_Action_Key AND A.Work_Center_Event_Identifier=B.Work_Center_Event_Identifier AND A.Sequence_Number=B.Sequence_Number AND A.Work_Order_Number=B.Work_Order_Number")
 
 
-    #Search for R&R in Corrective Narrative, replace corresponding ATC with "R"
+    # Search for R&R in Corrective Narrative, replace corresponding ATC with "R"
     Rs = df[df['Corrective_Narrative'].str.contains(pat=r"\br2\b|\br2\^d\b|\bremoved and replace|\br 2\b|\br&r\b|\br & r\b|\br and r\b|\br\+r\b|\br2d\b|\br2'd\b",flags=re.IGNORECASE)].index
     df.loc[Rs,'Action_Taken_Code'] = "R"
 
-    df=df[['On_Work_Order_Key','On_Maint_Action_Key','Work_Center_Event_Identifier','Sequence_Number','Work_Order_Number','Action_Taken_Code']]
+    # Remove ATCs we are not interested in
+    df = df[df.Action_Taken_Code.str.contains(r'R|P|Q|T|U')]
+
+    df = df[['On_Work_Order_Key','On_Maint_Action_Key','Work_Center_Event_Identifier','Sequence_Number','Work_Order_Number','Action_Taken_Code']]
+    
     return df    
 
 
