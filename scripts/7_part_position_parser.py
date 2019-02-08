643dbc6c196ff43379f1f17e607789c9db845e4b
tbaer-c7ks7s <tbaer@clockwork-solutions.com>
2019-02-08 14:25:35 -0600

    fix part position parser indentation

--- scripts/7_part_position_parser.py
+++ scripts/7_part_position_parser.py
@@ -19,9 +19,9 @@ def reader(df,libraries):
 
     #For each entry, search corrective,discrepancy narratives for component position numbers 
     for i in range (0,len(df)):
-    pounds = []
+        pounds = []
 
-    #For each entry, search corrective narrative for component position numbers
+        #For each entry, search corrective narrative for component position numbers
         if re.findall("\#[1-9]",df.loc[i,'Corrective_Narrative']) != []:
                 pounds = re.findall("\#[1-9]",df.loc[i,'Corrective_Narrative'])
 
@@ -64,7 +64,7 @@ def reader(df,libraries):
         elif re.findall("NUMBER [1-9][0-9]",df.loc[i,'Corrective_Narrative']) != []:
                 pounds =  re.findall("NUMBER [1-9][0-9]",df.loc[i,'Corrective_Narrative'])
             
-    #if no information is found in the corrective narrative, search discrepancy narrative
+        #if no information is found in the corrective narrative, search discrepancy narrative
         elif re.findall("\#[1-9]",df.loc[i,'Discrepancy_Narrative']) != []:
                 pounds = re.findall("\#[1-9]",df.loc[i,'Discrepancy_Narrative'])
 
@@ -107,41 +107,52 @@ def reader(df,libraries):
         elif re.findall("NUMBER [1-9][0-9]",df.loc[i,'Discrepancy_Narrative']) != []:
                 pounds =  re.findall("NUMBER [1-9][0-9]",df.loc[i,'Discrepancy_Narrative'])
 
-    #reformat string for ordering
-    string1 = str(pounds)
-    string1 = string1.replace("ALL 3","1,2,3")
-    string1 = string1.replace("ALL 4","1,2,3,4")
-    string1 = string1.replace("ALL THREE","1,2,3")
-    string1 = string1.replace("ALL FOUR","1,2,3,4")
-    string1 = string1.replace("\"","")
-    string1 = string1.replace("\'","")
-    string1 = string1.replace("#","")
-    string1 = string1.replace("NUMBER","")
-    string1 = string1.replace("NO.","")
-    string1 = string1.replace("NO","")
-    string1 = string1.replace(" ","")
-    string1 = string1.replace("[","")
-    string1 = string1.replace("]",",")
-    string1 = string1.strip(',')
+        #reformat string for ordering
+        string1 = str(pounds)
+        string1 = string1.replace("ALL 3","1,2,3")
+        string1 = string1.replace("ALL 4","1,2,3,4")
+        string1 = string1.replace("ALL THREE","1,2,3")
+        string1 = string1.replace("ALL FOUR","1,2,3,4")
+        string1 = string1.replace("\"","")
+        string1 = string1.replace("\'","")
+        string1 = string1.replace("#","")
+        string1 = string1.replace("NUMBER","")
+        string1 = string1.replace("NO.","")
+        string1 = string1.replace("NO","")
+        string1 = string1.replace(" ","")
+        string1 = string1.replace("[","")
+        string1 = string1.replace("]",",")
+        string1 = string1.strip(',')
 
-    #order numbers and reformat for output
-    string1 = str(sorted(string1.split(',')))
-    string1 = string1.replace("[","")
-    string1 = string1.replace("\'","")
-    string1 = string1.replace(" ","")
-    string1 = string1.replace("]","")
-    df.loc[i,'Parsed_Component_Position']=string1
+        #order numbers and reformat for output
+        string1 = str(sorted(string1.split(',')))
+        string1 = string1.replace("[","")
+        string1 = string1.replace("\'","")
+        string1 = string1.replace(" ","")
+        string1 = string1.replace("]","")
+        df.loc[i,'Parsed_Component_Position']=string1
 
-    #if no information is found in the narratives, copy in the provided 'Component_Position_Number'
+        #if no information is found in the narratives, copy in the provided 'Component_Position_Number'
         if df.loc[i,'Parsed_Component_Position'] == str():
             df.loc[i,'Parsed_Component_Position'] = df.loc[i,'Component_Position_Number']
+
     return df
 
 def fn(conn, libraries, params, predecessors):
 
     pd = libraries["pandas"]
     re = libraries["re"]
-    df = pd.read_sql(con=conn,sql="SELECT A.*, B.Discrepancy_Narrative, B.Corrective_Narrative, B.Component_Position_Number FROM clean_wuc_remove_fom A LEFT JOIN compiled_c130_remis_data B ON A.On_Work_Order_Key=B.On_Work_Order_Key AND A.On_Maint_Action_Key=B.On_Maint_Action_Key AND A.Work_Center_Event_Identifier=B.Work_Center_Event_Identifier AND A.Sequence_Number=B.Sequence_Number AND A.Work_Order_Number=B.Work_Order_Number")
+
+    for pred in predecessors:
+        if 'compiled' in pred:
+            compiled_table_name = pred
+            break
+
+    df = pd.read_sql(con=conn,sql="""SELECT A.*, B.Discrepancy_Narrative, B.Corrective_Narrative, B.Component_Position_Number FROM clean_wuc_remove_fom A 
+        LEFT JOIN {} B ON A.On_Work_Order_Key=B.On_Work_Order_Key AND A.On_Maint_Action_Key=B.On_Maint_Action_Key AND 
+        A.Work_Center_Event_Identifier=B.Work_Center_Event_Identifier AND A.Sequence_Number=B.Sequence_Number AND 
+        A.Work_Order_Number=B.Work_Order_Number""".format(compiled_table_name))
+
     df['Parsed_Component_Position']= ""
     df['Parsed_Component_Position'] = df['Parsed_Component_Position'].astype(str)
     df['Component_Position_Number'] = df['Component_Position_Number'].astype(str)
