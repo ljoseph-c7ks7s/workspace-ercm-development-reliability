1ea3adec2fc37b6c1f963bfe4fb12bc3cec8e5f4
tbaer-c7ks7s <tbaer@clockwork-solutions.com>
2019-03-21 14:38:35 -0500

    remove null serial number, entered in error, and duplicate discrepancy up front

--- scripts/4_Clean_WUC_Remove_FOM.py
+++ scripts/4_Clean_WUC_Remove_FOM.py
@@ -16,7 +16,7 @@ def fn(conn, libraries, params, predecessors):
     pd = libraries["pandas"]
     re = libraries["re"]    
     df = "***WRITE SQL HERE***"
-    df = pd.read_sql(con=conn,sql="SELECT A.*, B.Work_Center_Event_Narrative, B.Corrective_Narrative, B.Discrepancy_Narrative FROM wuc_edits A LEFT JOIN compiled_c130_remis_data B ON A.On_Work_Order_Key=B.On_Work_Order_Key AND A.On_Maint_Action_Key=B.On_Maint_Action_Key AND A.Work_Center_Event_Identifier=B.Work_Center_Event_Identifier AND A.Sequence_Number=B.Sequence_Number AND A.Work_Order_Number=B.Work_Order_Number")
+    df = pd.read_sql(con=conn,sql="SELECT A.*, B.Serial_Number, B.Work_Center_Event_Narrative, B.Corrective_Narrative, B.Discrepancy_Narrative FROM wuc_edits A LEFT JOIN compiled_c130_remis_data B ON A.On_Work_Order_Key=B.On_Work_Order_Key AND A.On_Maint_Action_Key=B.On_Maint_Action_Key AND A.Work_Center_Event_Identifier=B.Work_Center_Event_Identifier AND A.Sequence_Number=B.Sequence_Number AND A.Work_Order_Number=B.Work_Order_Number")
     pd = libraries["pandas"]
 
     print("The number of rows to start is: "+str(len(df)))
@@ -34,6 +34,26 @@ def fn(conn, libraries, params, predecessors):
     #Remove all other entries
     df=df.loc[keepum]
     print("The number of rows after removing FOMs is: "+str(len(df)))
+
+    # Find entries without 'entered in error' in corrective narrative
+    print("Entered in Error entries to remove: {}".format([df[df['Corrective_Narrative'].str.contains(pat=r'[AEIOU]NT\w*\s+[AEIOU]N\s+ER(R)?[AEIOU]R', na=False)]['Corrective_Narrative'].str.extract(pat=r'([AEIOU]NT\w*\s+[AEIOU]N\s+ER(R)?[AEIOU]R)')][0][0].unique()))
+    keepers = df[~df['Corrective_Narrative'].str.contains(pat=r'[AEIOU]NT\w*\s+[AEIOU]N\s+ER(R)?[AEIOU]R', na=False)].index
+    #Remove all other entries
+    df=df.loc[keepers]
+    print("The number of rows after removing ENTERED IN ERROR is: "+str(len(df)))
+
+    # Find entries without 'duplicate' in corrective narrative
+    print("Duplicate Entry entries to remove: {}".format([df[df['Corrective_Narrative'].str.contains(pat=r'(WRITE(\s+|-)UP|DISCR\w*)\s+DUPL\w*|DUPL\w*\s+(TAG|ENT\w*|DISCR\w*|WRITE-UP|WRITE UP)', na=False)]['Corrective_Narrative'].str.extract(pat=r'((WRITE(\s+|-)UP|DISCR\w*)\s+DUPL\w*|DUPL\w*\s+(TAG|ENT\w*|DISCR\w*|WRITE-UP|WRITE UP))')][0][0].unique()))
+    keepers = df[~df['Corrective_Narrative'].str.contains(pat=r'(WRITE(\s+|-)UP|DISCR\w*)\s+DUPL\w*|DUPL\w*\s+(TAG|ENT\w*|DISCR\w*|WRITE-UP|WRITE UP)', na=False)].index
+    #Remove all other entries
+    df=df.loc[keepers]
+    print("The number of rows after removing DUPLICATE ENTRY is: "+str(len(df)))
+
+    # Find entries with serial numbers
+    keepers = df[~df['Serial_Number'].isna()].index
+    #Remove all other entries
+    df=df.loc[keepers]
+    print("The number of rows after removing NULL Serial Numbers is: "+str(len(df)))
 
     #Remove all columns but primary keys for export
     df=df[['On_Work_Order_Key','On_Maint_Action_Key','Work_Center_Event_Identifier','Sequence_Number','Work_Order_Number']]
