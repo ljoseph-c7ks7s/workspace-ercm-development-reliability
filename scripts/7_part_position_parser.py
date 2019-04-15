304bc1312b7462745651f4e7f4afffa7162faaf9
tbaer-c7ks7s <tbaer@clockwork-solutions.com>
2019-04-14 22:09:37 -0500

    allow part position parser to use another predecessor table

--- scripts/7_part_position_parser.py
+++ scripts/7_part_position_parser.py
@@ -51,7 +51,7 @@ def reader(df,libraries):
             remove = ','.join(map(str, trim))
 
             # save values into df
-            df.at[i,'Parsed_Component_Position']=remove
+            df.loc[i,'Parsed_Component_Position']=remove
             
             # if empty, check next narrative
             if any(x.isdigit() for x in remove):
@@ -81,8 +81,8 @@ def fn(conn, libraries, params, predecessors):
     join_clause = ['A.{} = B.{}'.format(ii,ii) for ii in keys]
     join_clause = ' AND '.join(join_clause)
 
-    df = pd.read_sql(con=conn,sql="""SELECT A.*, B.Discrepancy_Narrative, B.Corrective_Narrative, B.Component_Position_Number FROM identify_r2_drop_atc A 
-        LEFT JOIN {} B ON {}""".format(compiled_table_name, join_clause))
+    df = pd.read_sql(con=conn,sql="""SELECT A.*, B.Discrepancy_Narrative, B.Corrective_Narrative, B.Component_Position_Number FROM {} A 
+        LEFT JOIN {} B ON {}""".format(key_table_name, compiled_table_name, join_clause))
 
     df['Parsed_Component_Position'] = ""
     df['Parsed_Component_Position'] = df['Parsed_Component_Position'].astype(str)
