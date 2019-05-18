bace9de6e0b3440ae919a64e883154c88ce97bb7
tbaer-c7ks7s <tbaer@clockwork-solutions.com>
2019-05-18 15:19:15 -0500

    add time to land date

--- components/Sortie_Accumulated_Hours.R
+++ components/Sortie_Accumulated_Hours.R
@@ -1,5 +1,6 @@
 fn <- function(conn_rmysql, load_path, params_path, component_path, output_file) {
   library(dplyr)
+  
   df_cumulate <- dbGetQuery(conn_rmysql, 'SELECT * FROM compiled_sortie_history_data')
   df_cumulate[df_cumulate[,"Depart_Date"] < as.Date('1950-01-01'), 5] <- df_cumulate[df_cumulate[,"Depart_Date"] < as.Date('1950-01-01'), 8]
   df_cumulate <- as.data.frame(df_cumulate)
@@ -12,11 +13,16 @@ fn <- function(conn_rmysql, load_path, params_path, component_path, output_file)
       Total_Landings = cumsum(Total_Landings),
       Full_Stop_Landings = cumsum(Full_Stop_Landings))
   
-  
   df_cumulate$Depart_Date <-  df_cumulate$Depart_Date %>% substr(1,10) %>% as.POSIXct()
   df_cumulate$Depart_Time <- substr(df_cumulate$Depart_Time %>% as.POSIXct(format = "%H:%M:%OS"),12,16)
   df_cumulate$Depart_Date <- as.POSIXct(paste(df_cumulate$Depart_Date, df_cumulate$Depart_Time), format = "%Y-%m-%d %H:%M")
   df_cumulate$Depart_Date <- df_cumulate$Depart_Date %>% as.character()
+
+  df_cumulate$Land_Date <-  df_cumulate$Land_Date %>% substr(1,10) %>% as.POSIXct()
+  df_cumulate$Land_Time <- substr(df_cumulate$Land_Time %>% as.POSIXct(format = "%H:%M:%OS"),12,16)
+  df_cumulate$Land_Date <- as.POSIXct(paste(df_cumulate$Land_Date, df_cumulate$Land_Time), format = "%Y-%m-%d %H:%M")
+  df_cumulate$Land_Date <- df_cumulate$Land_Date %>% as.character()
+  
   dbWriteTable(conn_rmysql, name = "sortie_accumulated_hours" , value = df_cumulate, 
                overwrite = FALSE, append = TRUE, row.names = FALSE)
 }
