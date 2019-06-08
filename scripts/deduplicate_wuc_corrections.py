fa354c01bb9e14b4e714ed97cae494ea3ab18104
tbaer-c7ks7s <tbaer@clockwork-solutions.com>
2019-06-08 14:02:16 -0500

    add hover to plot

--- scripts/deduplicate_wuc_corrections.py
+++ scripts/deduplicate_wuc_corrections.py
@@ -10,6 +10,7 @@ def fn(conn, libraries, params, predecessors):
      
     pd = libraries["pandas"]
     plotting = libraries["bokeh"]["plotting"]
+    models = libraries["bokeh"]["models"]
     io = libraries["bokeh"]["io"]
 
     df = pd.read_sql(sql="""SELECT * FROM {} p WHERE Work_Order_Number IS NOT NULL AND 
@@ -30,17 +31,33 @@ def fn(conn, libraries, params, predecessors):
     df_months = df.set_index('Posting_Date').groupby(pd.Grouper(freq='M')).size().reset_index()
     df_months.rename(columns={'Posting_Date':'Posting_Month', 0:'correction_count'}, inplace=True)
 
+    cds_months = plotting.ColumnDataSource(df_months)
     p = plotting.figure(title="Corrections Across Time", 
-        x_axis_type="datetime"
+        x_axis_type="datetime",
+           width=800,
+           tools='pan,wheel_zoom,box_zoom,reset,previewsave,hover'
         )
+    hover = p.select(dict(type=models.HoverTool))
+    hover.tooltips=[("month", "@Posting_Month{%F}"),
+        ("corrections", "@correction_count")]
+    hover.formatters={'Posting_Month': 'datetime'}
+    hover.mode='vline'
     p.xaxis.axis_label = 'Posting Date'
-    p.yaxis.axis_label = 'Corrections'
+    p.yaxis.axis_label = 'Corrections per Month'
     p.title.text_font_size = '18pt'
-    p.xaxis.axis_label_text_font_size = '12pt'
-    p.yaxis.axis_label_text_font_size = '12pt'
+    p.xaxis.axis_label_text_font_size = '14pt'
+    p.yaxis.axis_label_text_font_size = '14pt'
+    p.yaxis.axis_label_text_color = 'black'
+    p.xaxis.axis_label_text_color = 'black'
+    p.xaxis.major_label_text_font_size = '13pt'
+    p.yaxis.major_label_text_font_size = '13pt'
+    p.xaxis.major_label_text_color = 'black'
+    p.yaxis.major_label_text_color = 'black'
     p.toolbar.logo = None
-    p.line(x=df_months.Posting_Month,
-        y=df_months.correction_count
+    p.line(x='Posting_Month',
+        y='correction_count',
+           color='black',
+           source=cds_months
         )
 
     io.save(p)
