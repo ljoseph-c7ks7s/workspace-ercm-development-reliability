578a757d2adeda53ea3f9e9d067a27faa7285bf0
tbaer-c7ks7s <tbaer@clockwork-solutions.com>
2019-06-10 14:58:47 -0500

    add _more_ components

---
+++ models/20789122-1c7c-40e7-a774-60c54639fca5.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, Integer, BigInteger, String, DateTime, Text, Float, Date
+from sqlalchemy.dialects.mysql import DOUBLE, DECIMAL, TEXT, MEDIUMTEXT, MEDIUMBLOB, TINYINT, LONGTEXT, BINARY, SMALLINT, MEDIUMINT, BIT, TIMESTAMP, TIME
+from sqlalchemy import MetaData, Table
+
+metadata = MetaData()
+
+labeled_multiclass_removal_id = Table('labeled_multiclass_removal_id', metadata, 
+
+    Column('id', BigInteger, primary_key=True, autoincrement=False),
+    Column('Labels_File_Name', String(255), primary_key=False, autoincrement=False),
+    Column('REMIS_File_Name', String(255), primary_key=False, autoincrement=False),
+    Column('Model_Name', String(255), primary_key=False, autoincrement=False),
+    Column('Equipment_Designator', String(255), primary_key=False, autoincrement=False),
+    Column('Serial_Number', String(10), primary_key=False, autoincrement=False),
+    Column('Work_Unit_Code', String(5), primary_key=True, autoincrement=False),
+    Column('Transaction_Date', Date, primary_key=False, autoincrement=False),
+    Column('Geographic_Location', String(4), primary_key=False, autoincrement=False),
+    Column('Action_Taken_Code', String(1), primary_key=False, autoincrement=False),
+    Column('Discrepancy_Narrative', LONGTEXT, primary_key=False, autoincrement=False),
+    Column('Work_Center_Event_Narrative', LONGTEXT, primary_key=False, autoincrement=False),
+    Column('Corrective_Narrative', LONGTEXT, primary_key=False, autoincrement=False),
+    Column('train_test', String(45), primary_key=False, autoincrement=False),
+    Column('label', String(45), primary_key=False, autoincrement=False),
+    Column('prediction', String(45), primary_key=False, autoincrement=False),
+    Column('Matches', String(5), primary_key=False, autoincrement=False)
+)
