8778cd24f5eadf95c9bcf4aa152d2afb155f3cdd
devops <devops@clockwork-solutions.com>
2019-06-11 05:00:01 +0000

    customer updates

--- models/137b7585-62fe-44b5-8b4c-aa65fc72512b.py
+++ models/137b7585-62fe-44b5-8b4c-aa65fc72512b.py
@@ -4,7 +4,7 @@ from sqlalchemy import MetaData, Table
 
 metadata = MetaData()
 
-label_22tbb_action = Table('label_22tbb_action', metadata, 
+label_526dx_action = Table('label_526dx_action', metadata, 
 
     Column('Work_Order_Number', BigInteger, primary_key=True, autoincrement=False),
     Column('Work_Center_Event_Identifier', Integer, primary_key=True, autoincrement=False),
