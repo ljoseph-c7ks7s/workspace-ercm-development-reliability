1fd0aae6616c52df92985fcf07d7145c5c0d6741
tbaer-c7ks7s <tbaer@clockwork-solutions.com>
2019-06-13 11:25:51 -0500

    change component name for consistency

--- models/3236d9bb-5d8d-4b58-8fd5-ae088e2d6f0b.py
+++ models/3236d9bb-5d8d-4b58-8fd5-ae088e2d6f0b.py
@@ -4,7 +4,7 @@ from sqlalchemy import MetaData, Table
 
 metadata = MetaData()
 
-label_22vdx = Table('label_22vdx', metadata, 
+label_22vdx_action = Table('label_22vdx_action', metadata, 
 
     Column('Work_Order_Number', BigInteger, primary_key=True, autoincrement=False),
     Column('Work_Center_Event_Identifier', Integer, primary_key=True, autoincrement=False),
