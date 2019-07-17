4580cc4ba0e7692cd0c36cda48fe5de0c7e1e3f7
tbaer-c7ks7s <tbaer@clockwork-solutions.com>
2019-07-16 19:52:24 -0500

    add component to drop extraneous first-of-month sortie data

---
+++ models/784a9e6e-87b2-447d-a7be-c7d320f97085.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Integer, BigInteger, String, DateTime, Text, Float, Date
+from sqlalchemy.dialects.mysql import DOUBLE, DECIMAL, TEXT, MEDIUMTEXT, MEDIUMBLOB, TINYINT, LONGTEXT, BINARY, SMALLINT, MEDIUMINT, BIT, TIMESTAMP, TIME
+from sqlalchemy import MetaData, Table
+
+metadata = MetaData()
+
+sortie_import_old = Table('sortie_import_old', metadata, 
+
+    Column('MDS', String(6), primary_key=False, autoincrement=False),
+    Column('Serial_Number', String(15), primary_key=True, autoincrement=False),
+    Column('Mission_Number', String(20), primary_key=False, autoincrement=False),
+    Column('Sortie_Number', Integer, primary_key=False, autoincrement=False),
+    Column('Depart_ICAO', String(4), primary_key=False, autoincrement=False),
+    Column('Depart_Date', DateTime, primary_key=True, autoincrement=False),
+    Column('Depart_Time', TIME, primary_key=True, autoincrement=False),
+    Column('Land_ICAO', String(4), primary_key=False, autoincrement=False),
+    Column('Land_Date', DateTime, primary_key=False, autoincrement=False),
+    Column('Land_Time', TIME, primary_key=False, autoincrement=False),
+    Column('Flying_Hours', Float, primary_key=False, autoincrement=False),
+    Column('Sorties_Flown', Integer, primary_key=False, autoincrement=False),
+    Column('Total_Landings', Integer, primary_key=False, autoincrement=False),
+    Column('Full_Stop_Landings', Integer, primary_key=False, autoincrement=False),
+    Column('Sortie_Date', DateTime, primary_key=False, autoincrement=False),
+)
